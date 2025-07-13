0d37e4ea2e13f80c470c763678606f973e3b25a3
SeifElden Osama Hosney <157766725+SeifEldenOsama@users.noreply.github.com>
2025-07-13 10:20:02 +0300

    Update dataBaseConnection.java

--- DataBase/dataBaseConnection.java
+++ DataBase/dataBaseConnection.java
@@ -7,7 +7,7 @@ public class dataBaseConnection {
 	public static Connection connect() {
 	Connection con = null;
 	try {
-		con = DriverManager.getConnection("jdbc:mysql://localhost/Gallery?user=root&password=Seif2354296");
+		con = DriverManager.getConnection("jdbc:mysql://localhost/Gallery?user=root&password=***********");
 		System.out.println("Success");
 		return con;
 	}catch(Exception E) {
